bd734019610cf9dcdf42d43eb07d37abba8b95d2
Justin Chen <cxd032404@hotmail.com>
2020-04-02 16:29:09 +0800

    merge

--- app/Services/ElasticsService.php
+++ app/Services/ElasticsService.php
@@ -6,6 +6,7 @@ use Elasticsearch\ClientBuilder;
 class ElasticsService
 {
     private $client;
+    protected $documents = [];
 
     public function __construct()
     {
@@ -21,6 +22,26 @@ class ElasticsService
     public function search($params)
     {
         return $this->client->search($params);
+    }
+
+    /**
+     * @function Name addDocument
+     * @description 添加日志
+     * @param array $document
+     */
+    public function addDocument(array $document)
+    {
+        $this->documents[] = $document;
+    }
+
+    /**
+     * @function Name getDocuments
+     * @description 获取所有已添加日志
+     * @return mixed
+     */
+    public function getDocuments()
+    {
+        return $this->documents;
     }
 }
 
